1f406ff46e013de3078dc9b2063fcff6a60bf7b1
kapsomun <mazajko@gmail.com>
2023-06-16 20:06:24 +0300

    feat: add completed task in hash folder

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const currentFilePath = fileURLToPath(import.meta.url);
+const currentDirPath = dirname(currentFilePath);
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const fileData = await readFile(`${currentDirPath}/files/fileToCalculateHashFor.txt`);
+        const hash = createHash('sha256').update(fileData).digest('hex');
+        console.log(hash);
+    } catch (error) {
+        console.error(`Error reading the file: ${error}`);
+    }
 };
+
 
 await calculateHash();
