c1687922a64b2acd74ce3302372e5a0da09ae3ea
kapsomun <mazajko@gmail.com>
2023-06-16 20:11:03 +0300

    feat: add completed task in cli folder

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2)
+    const values = args.map((element, index, arr) => {
+        if (element.startsWith("--")) {
+            console.log(`${element.slice(2)} is ${arr[index + 1]};`);
+        }
+
+    })
+
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,6 +1,13 @@
+import { env } from 'node:process';
+
+
 const parseEnv = () => {
-    // Write your code here 
-    
-};
+    const prefix = "RSS_"
+    const keys = Object.keys(env).filter(key => key.startsWith(prefix));
+    const findedProperties = keys.map(e => {
+        console.log(`${e}=${env[e]};`)
+    })
+    return findedProperties
+}
 
 parseEnv();
